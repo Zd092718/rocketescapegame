4a1e85fb086ff2d03cb586f69f023e13d6665bed
Zachary Dowd <zacharyd2796@zohomail.com>
2022-03-24 15:32:38 -0600

    level progression works

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -9,11 +9,8 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 print("Hit friendly target");
                 break;
-            case "Fuel":
-                print("Hit fuel target");
-                break;
             case "Finish":
-                print("Hit finish target");
+                LoadNextLevel();
                 break;
             default:
                 ReloadScene();
@@ -25,6 +22,17 @@ public class CollisionHandler : MonoBehaviour
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
     
 }
